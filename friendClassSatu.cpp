4473d5c54d128dedba7ff40d7db4a69206b5a548
irfanalarib <irfanalarib7@gmail.com>
2025-05-05 16:27:25 +0700

    membuat function main

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -34,3 +34,12 @@ void siswa::displayAll(orang &a)
 {
     cout << id << endl << a.nama;
 }
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
